7e90ec1b76b26824e157bb28e617bcbfe3d69ebe
Makoto Kaneko <s.makoto.kaneko@iridge.jp>
2019-02-25 18:12:31 +0900

    add project for Xcode

---
+++ greenlife/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  greenlife
+//
+//  Created by makoto.kaneko on 2019/02/25.
+//  Copyright © 2019 makoto.kaneko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+
+}
+
